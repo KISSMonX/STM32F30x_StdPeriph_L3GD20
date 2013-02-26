5ed2c5bfd450b890b09f5c7f7e8e8fcfd2f8b17b
ebader <ebader@bannerengineering.com>
2013-02-26 17:15:35 -0600

    This is the master of that paoject

---
+++ main.c
@@ -0,0 +1,217 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/GPIO_Toggle/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    23-October-2012
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F30x_StdPeriph_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_Toggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+#define ABS(x)         (x < 0) ? (-x) : x
+
+#define L3G_Sensitivity_250dps     (float)   114.285f         /*!< gyroscope sensitivity with 250 dps full scale [LSB/dps] */
+#define L3G_Sensitivity_500dps     (float)    57.1429f        /*!< gyroscope sensitivity with 500 dps full scale [LSB/dps] */
+#define L3G_Sensitivity_2000dps    (float)    14.285f	      /*!< gyroscope sensitivity with 2000 dps full scale [LSB/dps] */
+#define PI                         (float)     3.14159265f
+
+/* Private variables ---------------------------------------------------------*/
+RCC_ClocksTypeDef RCC_Clocks;
+__IO uint32_t TimingDelay = 0;
+float GyroBuffer[3] = {0.0f};
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* SysTick end of count event each 10ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+  
+  /* Demo Gyroscope */
+  Demo_GyroConfig();
+    
+  while (1)
+  {
+    /* Read Gyro Angular data */
+    Demo_GyroReadAngRate(GyroBuffer);
+      
+    Delay(1000);
+  }
+}
+
+/**
+  * @brief  Configure the Mems to gyroscope application.
+  * @param  None
+  * @retval None
+  */
+void Demo_GyroConfig(void)
+{
+  L3GD20_InitTypeDef L3GD20_InitStructure;
+  L3GD20_FilterConfigTypeDef L3GD20_FilterStructure;
+  
+  /* Configure Mems L3GD20 */
+  L3GD20_InitStructure.Power_Mode = L3GD20_MODE_ACTIVE;
+  L3GD20_InitStructure.Output_DataRate = L3GD20_OUTPUT_DATARATE_1;
+  L3GD20_InitStructure.Axes_Enable = L3GD20_AXES_ENABLE;
+  L3GD20_InitStructure.Band_Width = L3GD20_BANDWIDTH_4;
+  L3GD20_InitStructure.BlockData_Update = L3GD20_BlockDataUpdate_Continous;
+  L3GD20_InitStructure.Endianness = L3GD20_BLE_LSB;
+  L3GD20_InitStructure.Full_Scale = L3GD20_FULLSCALE_500; 
+  L3GD20_Init(&L3GD20_InitStructure);
+   
+  L3GD20_FilterStructure.HighPassFilter_Mode_Selection =L3GD20_HPM_NORMAL_MODE_RES;
+  L3GD20_FilterStructure.HighPassFilter_CutOff_Frequency = L3GD20_HPFCF_0;
+  L3GD20_FilterConfig(&L3GD20_FilterStructure) ;  
+  
+  L3GD20_FilterCmd(L3GD20_HIGHPASSFILTER_ENABLE);
+}
+
+/**
+  * @brief  Calculate the angular Data rate Gyroscope.
+  * @param  pfData : Data out pointer
+  * @retval None
+  */
+void Demo_GyroReadAngRate (float* pfData)
+{
+  uint8_t tmpbuffer[6] ={0};
+  int16_t RawData[3] = {0};
+  uint8_t tmpreg = 0;
+  float sensitivity = 0;
+  int i =0;
+
+  L3GD20_Read(&tmpreg,L3GD20_CTRL_REG4_ADDR,1);
+  
+  L3GD20_Read(tmpbuffer,L3GD20_OUT_X_L_ADDR,6);
+  
+  /* check in the control register 4 the data alignment (Big Endian or Little Endian)*/
+  if(!(tmpreg & 0x40))
+  {
+    for(i=0; i<3; i++)
+    {
+      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i+1] << 8) + tmpbuffer[2*i]);
+    }
+  }
+  else
+  {
+    for(i=0; i<3; i++)
+    {
+      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i] << 8) + tmpbuffer[2*i+1]);
+    }
+  }
+  
+  /* Switch the sensitivity value set in the CRTL4 */
+  switch(tmpreg & 0x30)
+  {
+  case 0x00:
+    sensitivity=L3G_Sensitivity_250dps;
+    break;
+    
+  case 0x10:
+    sensitivity=L3G_Sensitivity_500dps;
+    break;
+    
+  case 0x20:
+    sensitivity=L3G_Sensitivity_2000dps;
+    break;
+  }
+  /* divide by sensitivity */
+  for(i=0; i<3; i++)
+  {
+    pfData[i]=(float)RawData[i]/sensitivity;
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in 10 ms.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
